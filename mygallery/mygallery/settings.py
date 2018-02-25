ef70f263a53797334a027a0ac382d6661439ab14
VickyMutai <vicky.mutai96@gmail.com>
2018-02-25 22:53:00 +0300

    sort to categories

--- mygallery/mygallery/settings.py
+++ mygallery/mygallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
     'bootstrap3',
+    'macros',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mygallery/photos/views.py
+++ mygallery/photos/views.py
@@ -4,7 +4,6 @@ from .models import Image,Category,Location
 
 # Create your views here.
 def home (request):
-    
     images = Image.get_images()
     return render(request,'home.html',
                   {"images":images})
