35d4a312bab4d2ecdd9580345a1461947c7964f1
VickyMutai <vicky.mutai96@gmail.com>
2018-02-25 13:21:31 +0300

    add location and category models

--- mygallery/photos/models.py
+++ mygallery/photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
