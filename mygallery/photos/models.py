b6fd3dacc3577aebc2d1b643bf2169acc428d564
VickyMutai <vicky.mutai96@gmail.com>
2018-02-25 13:32:08 +0300

    adding image models and migrating

--- mygallery/photos/models.py
+++ mygallery/photos/models.py
@@ -4,5 +4,25 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name    
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    location = models.ManyToManyField(Location)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['upload_date']
