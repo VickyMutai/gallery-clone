f9943ab683487a809606df0ec27d0ebc00e55c49
VickyMutai <vicky.mutai96@gmail.com>
2018-02-25 18:06:43 +0300

    display  images on index pages

--- mygallery/photos/views.py
+++ mygallery/photos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Image,Category
+from .models import Image,Category,Location
 
 # Create your views here.
 def home (request):
