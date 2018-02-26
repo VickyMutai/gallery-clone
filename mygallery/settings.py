0afc858074cd15389a5f4ef4193a903e587142f6
VickyMutai <vicky.mutai96@gmail.com>
2018-02-26 17:04:28 +0300

    before deployment

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5s72)6=z6znrj@$1(de#82j1km7-ye)=7r@klb=9xbv@&msg^2'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = []
 
